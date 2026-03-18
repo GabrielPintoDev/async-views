6573d928b8390e6739d8f042757f1c0bbd1ef8fc
Gabriel Andrade Pinto <gabrielandrade.colorado@gmail.com>
2026-03-18 18:45:14 -0300

    patching errors

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -3,7 +3,7 @@ import httpx
 from django.http import HttpResponse
 
 async def http_call_async():
-    for num in range (1, 6):
+    for num in range(1, 6):
         await asyncio.sleep(1)
         print(num)
     async with httpx.AsyncClient() as client:
@@ -11,6 +11,9 @@ async def http_call_async():
         print(r)
 
 async def async_view(request):
-    loop = asyncio.get_event_loop()
-    loop.create_task(http_call_async())
+    asyncio.create_task(http_call_async())
     return HttpResponse("Non-blocking HTTP request")
+
+async def http_call_async_view(request):
+    await http_call_async()
+    return HttpResponse("Finished async call")
